0ce88f1d3b01460ddd8a11d6328ea28d0e748042
piyushkumar2009 <savitamourya1988@gmail.com>
2024-05-20 14:55:37 +0530

    update

--- main.js
+++ main.js
@@ -1,21 +1,38 @@
-const input = document.getElementById('input');
+// Wait until the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+  // Get the elements by their IDs
+  const math = document.getElementById('maths');
+  const science = document.getElementById('science');
+  const sst = document.getElementById('sst');
+  const english = document.getElementById('english');
+  const hindi = document.getElementById('hindi');
+  const computer = document.getElementById('computer');
+  const output = document.getElementById('output');
+  const form = document.getElementById('form');
+  const maxMark = 600;
 
-const btn = document.getElementById('btn');
+  // Add an event listener to the form to handle the submit event
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); // Prevent the form from submitting the traditional way
 
-const tasks = document.querySelector('.tasks');
+    // Convert the values to numbers and sum them
+    const result = Number(math.value) + Number(science.value) + Number(sst.value) + Number(english.value) + Number(hindi.value) + Number(computer.value);
 
-const addTodo = () =>{
-  let pElem = document.createElement('p');
-  pElem.textContent = input.value;
-  tasks.append(pElem);
-  input.value='';
-}
+    // Calculate the percentage
+    let percentage = (result / maxMark) * 100;
 
-btn.addEventListener('click',()=>{
-  addTodo();
-})
+    // Format the percentage to two decimal places
+    let modifiedPercentage = percentage.toFixed(2);
 
-tasks.addEventListener('click',(event)=>{
-  let currentElem = event.target;
-  currentElem.remove();
-})
+    // Display the result
+    output.innerText = `You have scored ${result} out of ${maxMark} and your percentage is ${modifiedPercentage}`;
+    
+    math.value = '';
+    science.value = '';
+    english.value ='';
+    computer.value ='';
+    sst.value ='';
+    hindi.value='';
+    
+  });
+});
